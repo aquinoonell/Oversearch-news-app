82beebf05d1f045dea69de44b38c2167f235d505
don <onellaquino@icloud.com>
2025-05-19 20:57:52 -0400

    Terraform Integration 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,33 @@
 provider "aws" {
   region = "us-east-2"
-
-
 }
 
-terraform {
-  backend "s3" {
-    key = "none"
+## S3_Bucket
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enable = true
   }
+
 }
 
-module "alb" {
-  source = "terraform-aws/alb/aws"
-  version = "~> 6.0"
+## Lambda (store packages on S3)
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "lambda-with-layer"
+  description   = "My lambda function"
+  handler       = "index.lmabda_handler"
+  runtime       = "python3.12"
+  publish       = true
 
 }
